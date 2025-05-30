c8961e9de476bc5acfb9c0c6abb1a7053952c175
aident4 <aidentouchet4@gmail.com>
2025-05-30 16:52:39 +0000

    added to game platform, changed projectile to cannonball, add new platform and collectable

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -31,26 +31,28 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-    createPlatform(100, 700, 100, 10, "black")
-    createPlatform(600, 598, 100, 10, "black")
-    createPlatform(300, 500, 100, 10, "black")
-    createPlatform(600, 400, 100, 10, "black")
-    createPlatform(800, 300, 100, 10, "black")
-    createPlatform(1100, 200, 100, 10, "black")
+    createPlatform(100, 700, 100, 10, "black");
+    createPlatform(600, 598, 100, 10, "black");
+    createPlatform(300, 500, 100, 10, "black");
+    createPlatform(600, 400, 100, 10, "black");
+    createPlatform(800, 300, 100, 10, "black");
+    createPlatform(1100, 200, 100, 10, "black");
+    createPlatform(1300, 400, 100, 10, "black");
 
 
     // TODO 3 - Create Collectables
-    createCollectable("oreo", 300, 400, 0, 0)
-    createCollectable("oreo", 800, 200, 0, 0)
-    createCollectable("oreo", 1300, 700, 0, 0)
+    createCollectable("oreo", 300, 400, 0, 0);
+    createCollectable("oreo", 800, 200, 0, 0);
+    createCollectable("oreo", 1300, 700, 0, 0);
+    createCollectable("oreo", 1350, 300, 0, 0);
 
 
     
     // TODO 4 - Create Cannons
-    createCannon("top", 550, 950)
-    createCannon("top", 850, 600)
-    createCannon("top", 1375, 720)
-    createCannon("right", 800, 2000)
+    createCannon("top", 550, 950);
+    createCannon("top", 850, 600);
+    createCannon("top", 1375, 720);
+    createCannon("right", 800, 2000);
 
     
     
